441df331d132f1e73a3bf34a8d78398d1ab01fcf
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2025-09-28 01:05:38 -0600

    slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joaquinbadillo.github.io',
-  base: 'Euge',
+  base: '/Euge',
   integrations: [expressiveCode(expressiveCodeOptions), tailwind({
     applyBaseStyles: false
   }), sitemap(), mdx(), icon(), react()],
